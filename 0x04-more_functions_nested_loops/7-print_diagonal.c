e1f76887a814e97d61208ee4b271f458a995f2ed
Tsega2022 <tsgtmrt@gmail.com>
2022-09-15 15:28:41 -0700

    correction

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
-		_putchar('\n');
+		_putchar('36');
 	else
 	{
 		for (postn = 1; postn <= n; ++postn)
@@ -21,7 +21,7 @@ void print_diagonal(int n)
 				_putchar(' ');
 			_putchar(92); /*is equal to '/' char*/
 			_putchar(36); 
-			_putchar('\n');
+			_putchar('36');
 		}
 	}
 }
